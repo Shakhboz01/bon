c236877f6758295ef69a58bc4383284fdeb596b0
Shakhboz <shfazliddinov1940@gmail.com>
2024-05-11 16:25:45 +0500

    m

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -74,10 +74,11 @@ class SalesController < ApplicationController
 
     buyer = Buyer.find(params[:buyer_id])
     last_one = buyer.sales.order(created_at: :asc).last
-    if !last_one.nil? && !last_one.closed?
+    if !last_one.nil? && last_one.processing?
       if last_one.product_sells.empty?
         last_one.update(created_at: DateTime.current, user_id: current_user.id)
       end
+
       last_one.update(user_id: current_user.id)
       redirect_to sale_url(last_one)
     else
