ba82d9240b42c0b1fb8abe097feaef9c12d5d892
Shakhboz <Shfazliddinov1940@gmail.com>
2025-04-07 14:06:20 +0500

    some change

---
+++ app/interactions/product_sells/calculate_amount_per_pack.rb
@@ -0,0 +1,25 @@
+module ProductSells
+  class CalculateAmountPerPack < ActiveInteraction::Base
+    string :pack_name, default: nil
+    integer :amount, default: nil
+    integer :amount_per_pack, default: nil
+
+    def execute
+      return '' unless amount
+
+      self.amount_per_pack ||= Pack.find_by(name: pack_name)&.amount_per_pack if pack_name.present?
+      if amount < amount_per_pack
+        return "#{amount} ШТУК"
+      end
+
+      amount_in_box = (amount / amount_per_pack).to_i
+      remaining = (amount % amount_per_pack).to_i
+
+      if remaining.zero?
+        amount_in_box
+      else
+        "#{amount_in_box} УПАКОВКА И #{remaining} ШТУК"
+      end
+    end
+  end
+end
