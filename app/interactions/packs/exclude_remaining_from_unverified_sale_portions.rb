eaaeb410071bfefde2f1f76d62bedeb427755aa9
Shakhboz <Shfazliddinov1940@gmail.com>
2025-05-07 17:50:12 +0500

    m

--- app/interactions/packs/exclude_remaining_from_unverified_sale_portions.rb
+++ app/interactions/packs/exclude_remaining_from_unverified_sale_portions.rb
@@ -6,13 +6,18 @@ module Packs
 
       sales_out_of_sale_portions = Sale.where('created_at > ?', last_portion.till)
       # Step 1: Get all ranges
-      ranges = SalePortion.where(verified_by_factory: false).pluck(:from, :till)
+      ranges = SalePortion.where(verified_by_factory: false)
 
       # Step 2: Build a combined condition using OR for each range
       sales_scope = Sale.none
 
-      ranges.each do |from, till|
-        sales_scope = sales_scope.or(Sale.where(created_at: from..till))
+      ranges.each do |sale_portion|
+        if (user_id = sale_portion.user_id)
+          user = User.find(user_id)
+          sales_scope = sales_scope.or(Sale.where(created_at: sale_portion.from..sale_portion.till).where(agent_user: user))
+        else
+          sales_scope = sales_scope.or(Sale.where(created_at: sale_portion.from..sale_portion.till))
+        end
       end
 
       sales_scope = sales_scope.or(Sale.where(id: sales_out_of_sale_portions.pluck(:id)))
